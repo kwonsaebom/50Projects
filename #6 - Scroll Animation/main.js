7ceabadf6279fa19c5deef488bbc566225629734
Saebom <spring180@naver.com>
2024-09-11 17:26:01 +0900

    Scroll Animation - With JS

--- #6 - Scroll Animation/main.js
+++ #6 - Scroll Animation/main.js
@@ -1,7 +1,21 @@
-const boxes = document.querySelectorAll("box");
+const boxes = document.querySelectorAll(".box");
 
 window.addEventListener("scroll", checkBoxes);
-boxes.forEach((box) => {
-  box.classList.add("show");
-  console.log(box);
-});
+
+function checkBoxes() {
+  const triggerBottom = (window.innerHeight / 5) * 4;
+  console.log(triggerBottom);
+
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    console.log("bottom" + triggerBottom);
+    console.log("top" + boxTop);
+
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+}
