ee9dda518ff7f53c7e9ba73efc53eeb0ccb900db
Saebom <spring180@naver.com>
2024-09-12 17:10:40 +0900

    Split Landing Page - with JS (class)

--- #7 - Split Landing Page/main.js
+++ #7 - Split Landing Page/main.js
@@ -1,2 +1,8 @@
-const leftButton = document.querySelector(".container.left .button");
-const rightButton = document.querySelector(".container.right .button");
+const left = document.querySelector(".container.left");
+const right = document.querySelector(".container.right");
+
+left.addEventListener("mouseenter", () => left.classList.add("hover"));
+left.addEventListener("mouseleave", () => left.classList.remove("hover"));
+
+right.addEventListener("mouseenter", () => right.classList.add("hover"));
+right.addEventListener("mouseleave", () => right.classList.remove("hover"));
