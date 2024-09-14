e1eaa8cef0cbd886234f3f159ec6b3b8bf536fe0
Saebom <spring180@naver.com>
2024-09-14 22:51:35 +0900

    잔디

--- #8 - Form Input Wave/main.js
+++ #8 - Form Input Wave/main.js
@@ -1,14 +1,10 @@
 const formNode = document.querySelector(".form-container");
 const loginButton = document.querySelector(".login");
 
-const emailNode = document.querySelector("#email");
-const pwNode = document.querySelector("#password");
-
-const email = emailNode.value;
-const pw = pwNode.value;
-
 loginButton.addEventListener("submit", () => {
   if (email.trim().length === 0) {
+    alert();
+  } else {
     alert();
   }
 });
