d3fa5b7c22a571fc3570eea43cb54a2e300de295
Saebom <spring180@naver.com>
2024-09-20 16:21:45 +0900

    #10 - Dad Jokes (with Array)

--- #10 - Dad Jokes/main.js
+++ #10 - Dad Jokes/main.js
@@ -1,4 +1,12 @@
 const jokes = ["A", "B", "C", "D", "E"];
-const changeButton = document.querySelector("change");
+const changeButton = document.querySelector(".change");
+const joke = document.querySelector(".joke");
 
-changeButton.addEventListener("click", () => {});
+changeButton.addEventListener("click", () => {
+  let randIdx = Math.floor(Math.random() * jokes.length);
+
+  console.log(randIdx);
+  console.log(jokes[randIdx]);
+  console.log(joke);
+  joke.innerText = jokes[randIdx];
+});
