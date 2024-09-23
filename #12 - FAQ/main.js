82e1736fbb65bf1cb8c99c66448d522171d89836
Saebom <spring180@naver.com>
2024-09-23 17:46:08 +0900

    #10 - unfinished

---
+++ #12 - FAQ/main.js
@@ -0,0 +1,8 @@
+const openButtons = document.querySelectorAll(".open");
+const items = document.querySelectorAll(".item");
+
+openButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const activeItem = document.querySelector(".item.active");
+  });
+});
